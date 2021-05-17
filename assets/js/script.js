d4ae12f4ef1887f7d198302cf378db056729e05b
Kayla Dombrosky <kdombroskyy@live.com>
2021-05-17 17:30:13 -0400

    Handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+// prepare to add events to button by selecting the button by its id 
+// prepare to add <li> items to this <ul> by selecting the <ul> by its id
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 // placed before event listener. 
-var createTaskHandler = function() {
+// create a list item var listItemEl, assign it a class and content
+// add child <li> to <ul> by .appendChild 
+var createTaskHandler = function(event) {
+    // prevents browser from reloading page after form submission
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -11,4 +18,4 @@ var createTaskHandler = function() {
 }
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
