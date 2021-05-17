3e378a9b6424b1aac1468b54236e05a3a2326c2d
Kayla Dombrosky <kdombroskyy@live.com>
2021-05-17 18:36:14 -0400

    Capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,32 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-// placed before event listener. 
-// create a list item var listItemEl, assign it a class and content
-// add child <li> to <ul> by .appendChild 
+// placed before event listener to declare function before being called
 var createTaskHandler = function(event) {
     // prevents browser from reloading page after form submission
     event.preventDefault();
 
+    // selecting the input field, and targetting its value input by the user
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // add HTML div to <li>
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to column
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 
 formEl.addEventListener("submit", createTaskHandler);
